98109ff605cdc2b7820da16175f4c02d172f987d
Priyanshu_Agrawal <priyanshu100agrawal@gmail.com>
2023-09-05 07:13:30 +0530

    array problems

--- programs/apna college/8.4/sumOfSubArrays.cpp
+++ programs/apna college/8.4/sumOfSubArrays.cpp
@@ -2,8 +2,11 @@
 
 using namespace std; 
 
+
+
 int main(){
     int n =5;
+    int sum=11;
     int arr[n]={3,2,5,4,1};
 
     for(int i =0;i<n;i++){
@@ -11,7 +14,14 @@ int main(){
         for(int j=i;j<n;j++){
             sumOfSubArray+=arr[j];
             cout<<sumOfSubArray<<endl;
+
+            // to get the indexes 👇👇
+            // if(sumOfSubArray==sum){
+            //     cout<<i <<" "<<j;
+            //     return 1;
+            // }
         }
+        cout<<endl;
     }
     return 0;
 }
